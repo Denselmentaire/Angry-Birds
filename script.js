9de534a3524a489740ce45ad08dc49ecea1269cf
Desley Okx <90309350+Denselmentaire@users.noreply.github.com>
2021-09-24 10:51:04 +0000

    more buildings

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+var glass, wood, swidth
+
+swidth = 800;
+
 function setup() {
-	createCanvas(600, 400);
+	createCanvas(swidth, 400);
   background("cyan");
   
  
@@ -7,20 +11,23 @@ function setup() {
 
 function draw(){
   fill("#298f12");
-  rect(0,350,600,50);
+  rect(0,350,swidth,50);
   
   fill("#b1651f")
-  rect(300, 335, 210, 15)
-  rect(320, 250, 10, 85)
-  rect(400, 250, 10, 85)
-  rect(480, 250, 10, 85)
-  rect(320, 240, 85, 10)
-  rect(405, 240, 85, 10)
-
+  rect(500, 335, 210, 15)
+  rect(520, 250, 10, 85)
+  rect(600, 250, 10, 85)
+  rect(680, 250, 10, 85)
+  rect(520, 240, 85, 10)
+  rect(605, 240, 85, 10)
+  rect(530, 150, 150, 10)
+  ellipse(555, 130, 40, 40)
 
 
   fill("#ccfffe")
-  rect(350, 180, 10, 60)
+  rect(550, 160, 10, 80)
+  rect(650, 160, 10, 80)
+
 }
 
 
