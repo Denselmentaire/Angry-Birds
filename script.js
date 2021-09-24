754f3aa5b6a41783f14a3067ffd455860c5db620
Desley Okx <90309350+Denselmentaire@users.noreply.github.com>
2021-09-24 10:52:51 +0000

    more buildings

--- script.js
+++ script.js
@@ -2,11 +2,21 @@ var glass, wood, swidth
 
 swidth = 800;
 
-function setup() {
-	createCanvas(swidth, 400);
-  background("cyan");
-  
- 
+class Woodrec{
+  constructor(x, y, vx, vy, width, height, color){
+    this.color = "298f12"
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.height = height;
+    this.width = width; 
+  }
+
+  draw(){
+    rect(this.x, this.y, this.width, this.height);
+  }
+
 }
 
 function draw(){
@@ -21,13 +31,22 @@ function draw(){
   rect(520, 240, 85, 10)
   rect(605, 240, 85, 10)
   rect(530, 150, 150, 10)
+  rect(100,265, 15, 85)
   ellipse(555, 130, 40, 40)
 
 
   fill("#ccfffe")
   rect(550, 160, 10, 80)
   rect(650, 160, 10, 80)
+  ellipse(555, 130, 40, 40)
 
 }
 
 
+function setup() {
+	createCanvas(swidth, 400);
+  background("cyan");
+  
+  Rec1 = new Woodrec(500, 335, 210, 15)
+ 
+}
