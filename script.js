1b746935f8f5c02384e85f1a2a20891f4409c2e7
Desley Okx <90309350+Denselmentaire@users.noreply.github.com>
2021-10-13 07:34:16 +0000

    Menu

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var swidth, vx
+var swidth, sheight, vx, mode;
+
+sheight = 400;
 
 swidth = 1000;
 vx = 0;
 
 //Classes//
-class RedBird{
-  constructor(x, y, w, h, vx, vy, r){
+class RedBird {
+  constructor(x, y, w, h, vx, vy, r) {
     this.x = x;
     this.y = y;
     this.h = h;
@@ -15,16 +17,17 @@ class RedBird{
     this.vy = vy;
     this.r = r
   }
-  draw(){
 
-    ellipse(this.x, this.y,this.w, this.h, this.r);
+  draw() {
+
+    rect(this.x, this.y, this.w, this.h);
     this.x += this.vx;
     this.y += this.vy;
   }
 }
 
-class GlassRec{
-  constructor(x, y, w, h, c, vx, vy){
+class GlassEllip {
+  constructor(x, y, w, h, c, vx, vy) {
     this.x = x;
     this.y = y;
     this.h = h;
@@ -32,45 +35,43 @@ class GlassRec{
     this.clor = c;
     this.vx = vx;
     this.vy = vy;
-    
+
   }
 
-  draw(){
-    rect(this.x, this.y,this.w, this.h, this.c, this.vx, this.vy);
+  draw() {
+    ellipse(this.x, this.y, this.w, this.h, this.c, this.vx, this.vy);
   }
 }
 
-class GlassEllip{
-  constructor(x, y, w, h, c, vx, vy){
+class Block {
+  constructor(x, y, w, h, c, vx, vy) {
     this.x = x;
     this.y = y;
     this.h = h;
     this.w = w;
+    if(!c || c == ""){
+      c =  "#9e3700";
+    }
     this.clor = c;
     this.vx = vx;
     this.vy = vy;
-    
+
   }
 
-  draw(){
-    ellipse(this.x, this.y,this.w, this.h, this.c, this.vx, this.vy);
-  }
-}
-
-class Woodrec{
-  constructor(x, y, w, h, c, vx, vy){
-    this.x = x;
-    this.y = y;
-    this.h = h;
-    this.w = w;
-    this.clor = c;
-    this.vx = vx;
-    this.vy = vy;
-    
+  draw() {
+    fill(this.clor);
+    rect(this.x, this.y, this.w, this.h, this.c);
   }
 
-  draw(){
-    rect(this.x, this.y,this.w, this.h, this.c, this.vx, this.vy);
+  checkCollision(){
+    if(redb.x + redb.w > this.x){
+      // todo: niet rood maken maar blokje verwijderen? ofzo..
+      this.x = 1000;
+      
+    }
+    else{
+      this.x = this.x;
+    }
   }
 }
 
@@ -81,14 +82,19 @@ var rects = [];
 let bg;
 
 function setup() {
-	createCanvas(swidth, 400);
-  rec1 = new Woodrec(700, 335, 210, 15, "brown");
-  rec2 = new Woodrec(720, 250, 10, 85);
-  rec3 = new Woodrec(800, 250, 10, 85);
-  rec4 = new Woodrec(880, 250, 10, 85);
-  rec5 = new Woodrec(720, 240, 85, 10);
-  rec6 = new Woodrec(805, 240, 85, 10);
-  rec7 = new Woodrec(730, 150, 150, 10);
+  createCanvas(swidth, sheight);
+  mode = 0;
+  textSize(21);
+  rec1 = new Block(700, 335, 210, 15);
+  rec2 = new Block(720, 250, 10, 85);
+  rec3 = new Block(800, 250, 10, 85);
+  rec4 = new Block(880, 250, 10, 85);
+  rec5 = new Block(720, 240, 85, 10);
+  rec6 = new Block(805, 240, 85, 10);
+  rec7 = new Block(730, 150, 150, 10);
+
+  glassrec1 = new Block(750, 160, 10, 80, "lightblue");
+  glassrec2 = new Block(850, 160, 10, 80, "lightblue");
 
   rects.push(rec1);
   rects.push(rec2);
@@ -97,48 +103,61 @@ function setup() {
   rects.push(rec5);
   rects.push(rec6);
   rects.push(rec7);
-  
-  
+
+  rects.push(glassrec1);
+  rects.push(glassrec2);
+
+
   ellip1 = new GlassEllip(755, 130, 40, 40);
-  glassrec1 = new GlassRec(750, 160, 10, 80);
-  glassrec2 = new GlassRec(850, 160, 10, 80);
-  redb = new RedBird(235, 295, 25, 25,0,0);
+
+  redb = new RedBird(235, 295, 25, 25, 0, 0);
 }
 
 var lineY = 0;
 
+function keyPressed(){
+  if (keyCode === ENTER){
+    mode =1;
+  }
+}
 
-function draw(){
-  background("cyan");
+function draw() {
+  clear();
+
+  if (mode == 0){
+     background('white');
+    text('Angry birbs press enter',250, sheight / 2);
+  }
+
+  if (mode == 1){
+ background("cyan");
   fill("#298f12");
-  
-  rect(0,350,swidth,50);
+
+  rect(0, 350, swidth, 50);
 
   line(235, 295, mouseX, mouseY);
   //lineY++;
   // keyUp => lineY --;
   // keyDown => lineY ++;
 
-  
   //Wooden Recs//
   fill("#a56f36")
-  rects.forEach(r=>r.draw());
+  rects.forEach((r) => {
+    r.draw();
+    r.checkCollision();
+
+  });
 
   //Slingshot//
   rect(250, 285, 15, 65);
-  
-  
-
 
   fill("#f41e1e")
-  
+
   //Birds//
   redb.draw();
 
   //Glass Ellipse and Rectangles//
   fill("#ccfffe");
-  glassrec1.draw();
-  glassrec2.draw();
   ellip1.draw();
 }
 
@@ -148,5 +167,7 @@ function mouseClicked() {
   let ydist = redb.y - mouseY;
 
   let speed = xdist / redb.vx;
-  redb.vy = (ydist / speed) *-1;
+  redb.vy = (ydist / speed) * -1;
+  }
+ 
 }
